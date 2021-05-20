d5c1fe61e700cece83a6d6976df494c8c138e3fe
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2021-05-20 18:36:34 +0200

    Add tetrominos

--- tetris.js
+++ tetris.js
@@ -1,10 +1,79 @@
-const board = document.getElementById("board");
+const boardDiv = document.getElementById("board");
+const width = 10;
+const height = 20;
+const board = new Array(height);
 
-const boardArr = [];
-
-for (let i = 0; i < 200; i++) {
-  let div = document.createElement("div");
-  div.classList.add("square");
-  boardArr.push(div);
-  board.appendChild(div);
+// create the board array
+for (let i = 0; i < height; i++) {
+  board[i] = new Array(width);
+  for (let j = 0; j < width; j++) {
+    let div = document.createElement("div");
+    div.classList.add("square");
+    board[i][j] = div;
+    boardDiv.appendChild(div);
+  }
 }
+
+// the tetrominos
+const i = {
+  minos: [
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 0, 0],
+  ],
+  color: "cyan",
+};
+
+const j = {
+  minos: [
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 0],
+  ],
+  color: "blue",
+};
+
+const l = {
+  minos: [
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 1],
+  ],
+  color: "orange",
+};
+
+const o = {
+  minos: [
+    [1, 1],
+    [1, 1],
+  ],
+  color: "yellow",
+};
+
+const s = {
+  minos: [
+    [0, 0, 0],
+    [0, 1, 1],
+    [1, 1, 0],
+  ],
+  color: "green",
+};
+
+const z = {
+  minos: [
+    [0, 0, 0],
+    [1, 1, 0],
+    [0, 1, 1],
+  ],
+  color: "red",
+};
+
+const t = {
+  minos: [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+  ],
+  color: "purple",
+};
