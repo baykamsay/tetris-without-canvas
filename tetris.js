cb6c5cbbc1c5e6a240f5ccbe42b34820f4d8295f
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2021-05-20 19:31:47 +0200

    Write update function

--- tetris.js
+++ tetris.js
@@ -1,79 +1,104 @@
 const boardDiv = document.getElementById("board");
+
+// create board arrays for both the html elements and the logic
 const width = 10;
 const height = 20;
+const boardElements = new Array(height);
 const board = new Array(height);
 
-// create the board array
 for (let i = 0; i < height; i++) {
+  boardElements[i] = new Array(width);
   board[i] = new Array(width);
   for (let j = 0; j < width; j++) {
     let div = document.createElement("div");
     div.classList.add("square");
-    board[i][j] = div;
+    boardElements[i][j] = div;
     boardDiv.appendChild(div);
+    board[i][j] = 0;
   }
 }
 
 // the tetrominos
-const i = {
-  minos: [
-    [0, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 0, 0],
-    [0, 1, 0, 0],
-  ],
-  color: "cyan",
-};
+const i = [
+  [0, 1, 0, 0],
+  [0, 1, 0, 0],
+  [0, 1, 0, 0],
+  [0, 1, 0, 0],
+];
 
-const j = {
-  minos: [
-    [0, 1, 0],
-    [0, 1, 0],
-    [1, 1, 0],
-  ],
-  color: "blue",
-};
+const j = [
+  [0, 2, 0],
+  [0, 2, 0],
+  [2, 2, 0],
+];
 
-const l = {
-  minos: [
-    [0, 1, 0],
-    [0, 1, 0],
-    [0, 1, 1],
-  ],
-  color: "orange",
-};
+const l = [
+  [0, 3, 0],
+  [0, 3, 0],
+  [0, 3, 3],
+];
 
-const o = {
-  minos: [
-    [1, 1],
-    [1, 1],
-  ],
-  color: "yellow",
-};
+const o = [
+  [4, 4],
+  [4, 4],
+];
 
-const s = {
-  minos: [
-    [0, 0, 0],
-    [0, 1, 1],
-    [1, 1, 0],
-  ],
-  color: "green",
-};
+const s = [
+  [0, 0, 0],
+  [0, 5, 5],
+  [5, 5, 0],
+];
 
-const z = {
-  minos: [
-    [0, 0, 0],
-    [1, 1, 0],
-    [0, 1, 1],
-  ],
-  color: "red",
-};
+const t = [
+  [0, 0, 0],
+  [6, 6, 6],
+  [0, 6, 0],
+];
 
-const t = {
-  minos: [
-    [0, 0, 0],
-    [1, 1, 1],
-    [0, 1, 0],
-  ],
-  color: "purple",
-};
+const z = [
+  [0, 0, 0],
+  [7, 7, 0],
+  [0, 7, 7],
+];
+
+const tetrominos = [i, j, l, o, s, t, z];
+const colors = [
+  "black",
+  "cyan",
+  "blue",
+  "orange",
+  "yellow",
+  "green",
+  "purple",
+  "red",
+];
+
+// update game logic
+let curTime = 0; // current time
+let lastTime = 0; // last time updated
+let tickDuration = 800; // wait this much before updating
+
+function update(time = 0) {
+  const diff = time - lastTime;
+  curTime += lastTime;
+
+  if (lastTime > tickDuration) {
+    fall();
+  }
+  lastTime = time;
+  draw();
+  requestAnimationFrame(update);
+}
+
+function draw() {
+  board.forEach((row, y) => {
+    row.forEach((val, x) => {
+      boardElements[y][x].style.backgroundColor = colors[val];
+    });
+  });
+  // draw player too
+}
+
+function fall() {}
+
+update();
